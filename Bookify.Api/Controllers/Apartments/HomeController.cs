08565481f5efdbfe39ed19c3ac6c774d1a38847d
silas <silas.reis@e-construmarket.com.br>
2024-04-08 08:57:04 -0300

    Criação do projeto Api e Docker

---
+++ Bookify.Api/Controllers/Apartments/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Bookify.Application.Apartments.SearchApartments;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookify.Api.Controllers.Apartments;
+
+[ApiController]
+[Route("api/aypartments")]
+public class HomeController(ISender sender) : ControllerBase
+{
+    private readonly ISender _sender = sender;
+
+    [HttpGet]
+    public async Task<IActionResult> SearchApartments(
+        DateOnly startDate,
+        DateOnly endDate,
+        CancellationToken cancellationToken)
+    {
+        var query = new SearchApartmentsQuery(startDate, endDate);
+
+        var result = await _sender.Send(query, cancellationToken);
+
+        return Ok(result.Value);
+    }
+}
